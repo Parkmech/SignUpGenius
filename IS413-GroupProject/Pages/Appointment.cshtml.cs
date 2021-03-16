9f26f8fc279e96456e59b0fc826fdfa129037642
Logan-Hardy <70916374+Logan-Hardy@users.noreply.github.com>
2021-03-15 21:43:25 -0600

    Added Infrastructure and Pages folders

---
+++ IS413-GroupProject/Pages/Appointment.cshtml.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using IS413_GroupProject.Infrastructure;
+using IS413_GroupProject.Models;
+
+namespace IS413_GroupProject.Pages.Home
+{
+    public class AppointmentModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
